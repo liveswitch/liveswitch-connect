b12015ffbe951db49d4f47196f85a2fae0fabf10
Anton Venema <anton.venema@frozenmountain.com>
2021-01-14 13:15:29 -0800

    support for environment variables

--- src/FM.LiveSwitch.Connect/Program.cs
+++ src/FM.LiveSwitch.Connect/Program.cs
@@ -1,7 +1,9 @@
 ﻿using CommandLine;
 using CommandLine.Text;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
@@ -28,7 +30,7 @@ namespace FM.LiveSwitch.Connect
                 LogOptions,
                 RecordOptions,
                 InterceptOptions
-            >(args);
+            >(AppendEnvironmentVariables(args));
 
             result.MapResult(
                 (ShellOptions options) =>
@@ -157,6 +159,88 @@ namespace FM.LiveSwitch.Connect
             }
 
             Log.AddProvider(new ErrorLogProvider(options.LogLevel));
+        }
+
+        private static string[] AppendEnvironmentVariables(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return args;
+            }
+
+            var verb = args[0];
+            if (!TryGetOptions(verb, out var options))
+            {
+                return args;
+            }
+
+            var newArgs = new List<string>(args);
+            foreach (var unusedOption in FilterOptions(args, options))
+            {
+                var value = Environment.GetEnvironmentVariable($"{Assembly.GetExecutingAssembly().GetName().Name.ToUpperInvariant()}_{verb.ToUpperInvariant()}_{unusedOption.LongName.ToUpperInvariant()}");
+                if (value != null)
+                {
+                    Console.Error.WriteLine($"Environment variable discovered matching --{unusedOption.LongName} option.");
+                    newArgs.Add($"--{unusedOption.LongName}={value}");
+                }
+            }
+            return newArgs.ToArray();
+        }
+
+        private static string Environmentalize(string name)
+        {
+            return name.ToUpperInvariant().Replace("-", "_").Replace(".", "_");
+        }
+
+        private static bool TryGetOptions(string verb, out OptionAttribute[] options)
+        {
+            foreach (var type in Assembly.GetExecutingAssembly().GetTypes().Where(type => !type.IsAbstract))
+            {
+                var verbAttribute = type.GetCustomAttributes<VerbAttribute>().FirstOrDefault();
+                if (verbAttribute != null)
+                {
+                    if (verbAttribute.Name == verb)
+                    {
+                        options = type.GetProperties()
+                            .Select(property => property.GetCustomAttributes<OptionAttribute>().FirstOrDefault())
+                            .Where(option => option != null).ToArray();
+                        return true;
+                    }
+                }
+            }
+
+            options = null;
+            return false;
+        }
+
+        private static OptionAttribute[] FilterOptions(string[] args, OptionAttribute[] options)
+        {
+            var usedLongNames = new HashSet<string>();
+            var usedShortNames = new HashSet<string>();
+
+            foreach (var arg in args)
+            {
+                if (arg.StartsWith("--"))
+                {
+                    var longName = arg.Substring(2);
+                    if (longName.Contains('='))
+                    {
+                        longName = longName.Substring(0, longName.IndexOf('='));
+                    }
+                    usedLongNames.Add(longName);
+                }
+                else if (arg.StartsWith("-"))
+                {
+                    var shortName = arg.Substring(1);
+                    if (shortName.Contains('='))
+                    {
+                        shortName = shortName.Substring(0, shortName.IndexOf('='));
+                    }
+                    usedShortNames.Add(shortName);
+                }
+            }
+
+            return options.Where(option => !usedLongNames.Contains(option.LongName) && !usedShortNames.Contains(option.ShortName)).ToArray();
         }
     }
 }
